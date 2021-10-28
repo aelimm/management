1e95b12d91904f879299e0741c831607fd447292
aelimm <91526066+aelimm@users.noreply.github.com>
2021-10-28 19:15:48 +0900

    management

--- app/src/main/java/com/example/ng/MainActivity.java
+++ app/src/main/java/com/example/ng/MainActivity.java
@@ -5,15 +5,34 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
     //github push test1
 
+    private Button sendbt;
+
+    private FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+    private DatabaseReference databaseReference = firebaseDatabase.getReference();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sendbt = (Button) findViewById(R.id.button);
+
+        sendbt.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                // 버튼 누르면 수행 할 명령
+                databaseReference.child("message").push().setValue("2");
+                databaseReference.child("message").child("gbgg").setValue("2");
+            }
+        });
     }
     public void goFoodList(View view) {
         Intent myIntent = new Intent(getApplicationContext(), FoodListView.class);
@@ -24,4 +43,5 @@ public class MainActivity extends AppCompatActivity {
         Intent myIntent = new Intent(getApplicationContext(), PillListView.class);
         startActivity(myIntent);
     }
+
 }
